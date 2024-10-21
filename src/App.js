84b4ecba676503897a9f20f4ed6e3750d984ba07
Yuliia <fannciful@MacBook-Air---Yuliia.local>
2024-10-21 03:03:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,76 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      emojis: [
-        { id: 1, emoji: "😃", votes: 0 },
-        { id: 2, emoji: "😊", votes: 0 },
-        { id: 3, emoji: "😎", votes: 0 },
-        { id: 4, emoji: "🤩", votes: 0 },
-        { id: 5, emoji: "😍", votes: 0 }
-      ],
-      winner: null
-    };
-  }
+const App = () => {
+  const [emojis, setEmojis] = useState([
+    { id: 1, emoji: "😃", votes: 0 },
+    { id: 2, emoji: "😊", votes: 0 },
+    { id: 3, emoji: "😎", votes: 0 },
+    { id: 4, emoji: "🤩", votes: 0 },
+    { id: 5, emoji: "😍", votes: 0 }
+  ]);
 
-  componentDidMount() {
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
     const storedVotes = localStorage.getItem('votes');
     if (storedVotes) {
-      this.setState({ emojis: JSON.parse(storedVotes) });
+      setEmojis(JSON.parse(storedVotes));
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.emojis !== this.state.emojis) {
-      localStorage.setItem('votes', JSON.stringify(this.state.emojis));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('votes', JSON.stringify(emojis));
+  }, [emojis]);
 
-  handleVote = (id) => {
-    const updatedEmojis = this.state.emojis.map(emoji =>
+  const handleVote = (id) => {
+    const updatedEmojis = emojis.map(emoji =>
       emoji.id === id ? { ...emoji, votes: emoji.votes + 1 } : emoji
     );
-    this.setState({ emojis: updatedEmojis });
+    setEmojis(updatedEmojis);
   };
 
-  showResults = () => {
-    const maxVotes = Math.max(...this.state.emojis.map(emoji => emoji.votes));
-    const topEmoji = this.state.emojis.find(emoji => emoji.votes === maxVotes);
-    this.setState({ winner: topEmoji });
+  const showResults = () => {
+    const maxVotes = Math.max(...emojis.map(emoji => emoji.votes));
+    const topEmoji = emojis.find(emoji => emoji.votes === maxVotes);
+    setWinner(topEmoji);
   };
 
-  resetResults = () => {
-    const resetEmojis = this.state.emojis.map(emoji => ({ ...emoji, votes: 0 }));
-    this.setState({ emojis: resetEmojis, winner: null });
+  const resetResults = () => {
+    const resetEmojis = emojis.map(emoji => ({ ...emoji, votes: 0 }));
+    setEmojis(resetEmojis);
+    setWinner(null);
     localStorage.setItem('votes', JSON.stringify(resetEmojis));
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Голосування за найкращий смайлик</h1>
-        <div className="emoji-list">
-          {this.state.emojis.map((emoji) => (
-            <div key={emoji.id} className="emoji-item" onClick={() => this.handleVote(emoji.id)}>
-              <span className="emoji">{emoji.emoji}</span>
-              <span className="votes">{emoji.votes}</span>
-            </div>
-          ))}
-        </div>
-        <button onClick={this.showResults}>Show Results</button>
-        {this.state.winner && (
-          <div>
-            <h2>Переможець: {this.state.winner.emoji}</h2>
-            <p>Кількість голосів: {this.state.winner.votes}</p>
+  return (
+    <div className="App">
+      <h1>Голосування за найкращий смайлик</h1>
+      <div className="emoji-list">
+        {emojis.map((emoji) => (
+          <div key={emoji.id} className="emoji-item" onClick={() => handleVote(emoji.id)}>
+            <span className="emoji">{emoji.emoji}</span>
+            <span className="votes">{emoji.votes}</span>
           </div>
-        )}
-        <button onClick={this.resetResults}>Очистити результати</button>
+        ))}
       </div>
-    );
-  }
-}
+      <button onClick={showResults}>Show Results</button>
+      {winner && (
+        <div>
+          <h2>Переможець: {winner.emoji}</h2>
+          <p>Кількість голосів: {winner.votes}</p>
+        </div>
+      )}
+      <button onClick={resetResults}>Очистити результати</button>
+    </div>
+  );
+};
 
 export default App;
